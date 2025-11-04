2dbe7d3a534ab6f59d0d50b803ac6249665e5f27
Sunjay Kumar <ak2604745@gmail.com>
2025-11-04 19:54:07 +0500

    feat: config docker

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import ChatPage from './page/ChatPage';
 
 const App = () => {
   return (
-    <div className="min-h-screen bg-natural-900 text-white ">
+    <div className="min-h-screen bg-neutral-900 text-white ">
       <ChatPage />
     </div>
   );
